eb32cb80ac34f024a858f62b2256f504c6c8cab2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-24 21:00:33 -0300

    feat: add files

---
+++ src/test/java/com/github/acnaweb/study_apiw/StudyApiwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.acnaweb.study_apiw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
